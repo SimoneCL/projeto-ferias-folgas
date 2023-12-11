123e21e96517f82766c038c0ad920a11681ebed3
Simone Cristine Langer <simone.cristine@totvs.com.br>
2023-12-11 08:47:37 -0300

    ajusteTooltilp

--- src/app/ferias-folga/ferias-folga.component.ts
+++ src/app/ferias-folga/ferias-folga.component.ts
@@ -193,7 +193,7 @@ export class FeriasFolgaComponent implements OnInit, OnDestroy {
 
       { property: 'idUsuario', label: 'idUsuario', type: 'number', visible: false },
       {
-        property: 'nomeUsuario', label: 'Nome', width: '200px',type: 'link' , action: (value, row) => {
+        property: 'nomeUsuario', label: 'Nome', width: '200px',type: 'link' , tooltip: this.literals.newEventUser, action: (value, row) => {
         this.redirect(value, row);
       }
       }];
